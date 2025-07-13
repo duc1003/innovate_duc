4cbab438ea36a5d2799bf14225e6b185e2416d24
ducc0506 <ducnd.ntd.vn@gmail.com>
2025-07-13 11:13:37 +0700

    edit some files configtion

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
+    tailwindcss(),
     react(),
     mode === 'development' &&
     componentTagger(),
