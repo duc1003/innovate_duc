967798bd80f7c3b92c4ae0a5cb381fdced30638b
ducc0506 <ducnd.ntd.vn@gmail.com>
2025-07-13 16:32:25 +0700

    init based

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
